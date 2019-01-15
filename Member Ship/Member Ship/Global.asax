71a7ba4d31c93c5e6164c38d8b47fd356074e550
mustafa magdy <mustafa.magdy1111@gmail.com>
2019-01-14 15:27:19 -1200

    First Intial

---
+++ Member Ship/Member Ship/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Member_Ship.MvcApplication" Language="C#" %>
---
+++ Member Ship/Member Ship/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Member_Ship.Startup))]
+namespace Member_Ship
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
